5568770ecc72829a105ff00d80401c592ba3a3b5
Tobias Dosdal-Feddersen <kontakt@tobiasdosdal.dk>
2024-11-26 11:23:00 +0100

    Start filer

---
+++ src/Person.java
@@ -0,0 +1,22 @@
+public abstract class Person
+{
+    protected String name;
+    protected String email;
+    protected String telefon;
+    protected String adresse;
+
+    public Person(String name, String email, String telefon, String adresse)
+    {
+        this.name = name;
+        this.email = email;
+        this.telefon = telefon;
+        this.adresse = adresse;
+    }
+
+    public void opdaterKontaktinfo()
+    {
+
+    }
+
+
+}
