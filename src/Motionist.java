2ddae6cbcef52eef01443dc6182e9c10f8f30d76
Line <lijo0004@stud.kea.dk>
2024-11-27 17:10:52 +0100

    hold update

---
+++ src/Motionist.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Motionist {
+    private String niveau;
+    //ArrayList<>interresser;
+    private boolean harInstruktur;
+
+    public Motionist (String niveau, boolean harInstruktur) {
+        this.harInstruktur = harInstruktur;
+        this.niveau = niveau;
+    }
+}
