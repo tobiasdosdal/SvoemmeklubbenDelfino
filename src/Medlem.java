49872ed6ede7b841bc0546161046e973e264e61a
Lymse25 <lymae01@gmail.com>
2024-11-29 14:18:26 +0100

    Nye medlemmer

--- src/Medlem.java
+++ src/Medlem.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Medlem extends Person {
     private int medlemsId;
@@ -43,8 +45,31 @@ public class Medlem extends Person {
     // Override toString for at inkludere medlems-specifik information
     @Override
     public String toString() {
-        return super.toString() +
-                String.format(", MedlemsID: %d, Alder: %d, Aktiv: %b",
-                        medlemsId, alder, erAktiv);
+        return String.format("Navn: %s, Email: %s, Telefon: %s, Adresse: %s, MedlemsID: %d, Alder: %d, Aktiv: %b",
+                name, email, telefon, adresse, medlemsId, alder, erAktiv);
+    }
+
+    // Metode til at oprette hardcodede medlemmer
+    public static List<Medlem> opretMedlemmer() {
+        List<Medlem> medlemmer = new ArrayList<>();
+        medlemmer.add(new Medlem("Mads Mikkelsen", "mads@mikkelsen.dk", "12345678", "Frederiksberg Allé 10, 1820 Frederiksberg", 1, 57, true));
+        medlemmer.add(new Medlem("Sofie Gråbøl", "sofie@gråbøl.dk", "87654321", "Østerbro 5, 2100 København Ø", 2, 55, true));
+        medlemmer.add(new Medlem("Lars Mikkelsen", "lars@mikkelsen.dk", "11223344", "Nørrebrogade 20, 2200 København N", 3, 59, false));
+        medlemmer.add(new Medlem("Birgitte Hjort Sørensen", "birgitte@hjortsorensen.dk", "44332211", "Amagerbrogade 15, 2300 København S", 4, 41, true));
+        medlemmer.add(new Medlem("Nikolaj Coster-Waldau", "nikolaj@costerwaldau.dk", "55667788", "Valby Langgade 30, 2500 Valby", 5, 53, true));
+        medlemmer.add(new Medlem("Sidse Babett Knudsen", "sidse@babettknudsen.dk", "77889900", "Christianshavn 25, 1400 København K", 6, 54, false));
+        medlemmer.add(new Medlem("Casper Christensen", "casper@christensen.dk", "88990011", "Hellerupvej 45, 2900 Hellerup", 7, 54, true));
+        medlemmer.add(new Medlem("Nikolaj Lie Kaas", "nikolaj@liekaas.dk", "99001122", "Gentofte Parkvej 15, 2820 Gentofte", 8, 51, true));
+        medlemmer.add(new Medlem("Trine Dyrholm", "trine@dyrholm.dk", "11002233", "Vesterbrogade 8, 1620 København V", 9, 52, false));
+        medlemmer.add(new Medlem("Rasmus Bjerg", "rasmus@bjerg.dk", "22003344", "Islands Brygge 10, 2300 København S", 10, 47, true));
+        return medlemmer;
+    }
+
+    // Main metode til at teste og vise medlemmerne
+    public static void main(String[] args) {
+        List<Medlem> medlemmer = opretMedlemmer(); // Kalder metoden for at oprette medlemmer
+        for (Medlem medlem : medlemmer) {
+            System.out.println(medlem); // Printer hvert medlem
+        }
     }
 }
